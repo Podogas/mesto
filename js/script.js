ed15b9b151846e0e84c224e2ab62c0959caa231b
Alexandr Podogas <a.podogas@gmail.com>
2020-05-16 08:46:32 +0300

    исправить ошибки после первого ревью

--- js/script.js
+++ js/script.js
@@ -1,43 +1,50 @@
-let profileEditBtn = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+const profileEditBtn = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const profileForm = document.querySelector('.popup__input-container');
+const profileNameInput = profileForm.querySelector('.popup__input-item_el_name');
+const profileJobInput = profileForm.querySelector('.popup__input-item_el_job');
+const closePopupBtn = document.querySelector('.popup__close-btn');
+const profileSubmitBtn = profileForm.querySelector('.popup__submit-btn');
+/*элементы DOM*/
+
 
 let profile = {
   name: document.querySelector('.profile__name'),
   job: document.querySelector('.profile__job')
 }
+/*это объект с информацией о пользователе*/
 
 
-function openPopup() {
-  let profileForm = document.querySelector('.profile-form__input-container'); 
-  let profileNameInput = profileForm.querySelector('.profile-form__item_el_name');
-  let profileJobInput = profileForm.querySelector('.profile-form__item_el_job');
-  let closePopupBtn = document.querySelector('.profile-form__close-btn');
-  let profileSubmitBtn = profileForm.querySelector('.profile-form__submit-btn');
-/*  не уверен где лучше хранить эти переменные, но решил тут, 
-  раз уж эти элементы у нас появляются на экране только при вызове функции. 
-  Так же не знаю как это скажется на скорости открытия попапа.
-*/  
-  profileNameInput.setAttribute('value', profile.name.textContent);
-  profileJobInput.setAttribute('value', profile.job.textContent); 
-  popup.classList.add('popup_opened'); 
-  closePopupBtn.addEventListener('click', closePopup);
-  profileEditBtn.removeEventListener('click', openPopup);  
-  function closePopup() {
-    popup.classList.remove('popup_opened');
-    profileEditBtn.addEventListener('click', openPopup);
-    closePopupBtn.removeEventListener('click', closePopup);
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    closePopupBtn.addEventListener('click', togglePopup);
+    profileEditBtn.removeEventListener('click', togglePopup);
+    closePopupBtn.addEventListener('click', togglePopup);
+    profileNameInput.setAttribute('value', profile.name.textContent);
+    profileJobInput.setAttribute('value', profile.job.textContent);
   }
-  function formSubmitHandler (sendProfileForm) {
-    sendProfileForm.preventDefault();
+  /*условие нужно для того что бы удалять и добавлять EventListener*/ 
+  else {
+    profileEditBtn.addEventListener('click', togglePopup);
+    closePopupBtn.removeEventListener('click', togglePopup);
+  }
+}      
+/*Не получается решить проблему с текстовыми значениями в инпутах
+суть следующая: я ввожу значение в поле, нажимаю закрыть, value остается как и был
+но при повторном открытии введенный текст отсается на своем месте, 
+не смог найти способов его заставить при нажатии принимать значение value*/
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
     profile.name.textContent = profileNameInput.value;
     profile.job.textContent = profileJobInput.value;
-    closePopup();
-  }
+    togglePopup();
+} 
 
-profileForm.addEventListener('submit', formSubmitHandler);
-}
+profileEditBtn.addEventListener('click', togglePopup);
 
-profileEditBtn.addEventListener('click', openPopup);
-
-
-
+profileSubmitBtn.addEventListener('click', formSubmitHandler);
+/*Как удалить этот EventListener я не придумал, сейчас получается что он всегда висит на странице насколько это критично?
+И как обычно поступают в таких случаях, в интернетах есть те кто топят за удаление тк EventListener нагружает страницу,
+ а есть те кто говорят что можно оставить*/
