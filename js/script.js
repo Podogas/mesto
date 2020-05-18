822f0863b64a9016bfb1e08a4062526b4a3a6180
Alexandr Podogas <a.podogas@gmail.com>
2020-05-18 03:01:12 +0300

    исправить ошибки после второго ревью

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const profileSubmitBtn = profileForm.querySelector('.popup__submit-btn');
 /*элементы DOM*/
 
 
-let profile = {
+const profile = {
   name: document.querySelector('.profile__name'),
   job: document.querySelector('.profile__job')
 }
@@ -18,33 +18,26 @@ let profile = {
 function togglePopup() {
   popup.classList.toggle('popup_opened');
   if (popup.classList.contains('popup_opened')) {
-    closePopupBtn.addEventListener('click', togglePopup);
-    profileEditBtn.removeEventListener('click', togglePopup);
-    closePopupBtn.addEventListener('click', togglePopup);
-    profileNameInput.setAttribute('value', profile.name.textContent);
-    profileJobInput.setAttribute('value', profile.job.textContent);
-  }
-  /*условие нужно для того что бы удалять и добавлять EventListener*/ 
-  else {
-    profileEditBtn.addEventListener('click', togglePopup);
-    closePopupBtn.removeEventListener('click', togglePopup);
-  }
+   profileNameInput.value = profile.name.textContent;
+   profileJobInput.value = profile.job.textContent; 
+  } 
 }      
-/*Не получается решить проблему с текстовыми значениями в инпутах
-суть следующая: я ввожу значение в поле, нажимаю закрыть, value остается как и был
-но при повторном открытии введенный текст отсается на своем месте, 
-не смог найти способов его заставить при нажатии принимать значение value*/
 
 function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profile.name.textContent = profileNameInput.value;
-    profile.job.textContent = profileJobInput.value;
-    togglePopup();
-} 
+  evt.preventDefault();
+
+ if  ((profileNameInput.value.trim().length > 0) && (profileJobInput.value.trim().length > 0)) {
+        profile.name.textContent = profileNameInput.value;
+        profile.job.textContent = profileJobInput.value;
+        togglePopup();
+      } else {
+        alert("Пожалуйста заполните оба поля");
+      }
+/*добавил проверку на пустые значения, по какой то причине перестал работать required в поле ввода.
+не понял кстати почему. Оформление ошибки конечно так себе(alert) , и классно было бы вынести это
+в отдельную функцию определения пустой строки, но надо бы продолжить учиться,все таки новый спринт начался */ 
+  } 
 
 profileEditBtn.addEventListener('click', togglePopup);
-
+closePopupBtn.addEventListener('click', togglePopup);
 profileSubmitBtn.addEventListener('click', formSubmitHandler);
-/*Как удалить этот EventListener я не придумал, сейчас получается что он всегда висит на странице насколько это критично?
-И как обычно поступают в таких случаях, в интернетах есть те кто топят за удаление тк EventListener нагружает страницу,
- а есть те кто говорят что можно оставить*/
