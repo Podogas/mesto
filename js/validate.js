4c89b06e68082f66353ef82f082e81cf97bf4034
Alexandr Podogas <a.podogas@gmail.com>
2020-07-17 05:36:26 +0300

    second review of 7th project

--- js/validate.js
+++ js/validate.js
@@ -3,22 +3,24 @@ const inputError2 = document.querySelector(`#${popupInput2.id}-err`);
 /*обявляем ошибки именно в этом файле, так как они нигде кроме как тут не будут использоваться(скорее всего)*/
 
   function enableValidation(inputs) {
-    inputError1.textContent = '';
-    inputError2.textContent = '';
-    popupInput1.classList.remove('popup__input-item_invalid')
-    popupInput2.classList.remove('popup__input-item_invalid');
-    if(document.activeElement=== popupInput1 || document.activeElement=== popupInput2){
-      popupInput1.classList.add('popup__input-item_invalid');
-      inputError1.textContent = popupInput1.validationMessage;
-      if (popupInput1.validationMessage === ''){
-        popupInput1.classList.remove('popup__input-item_invalid');
-      }
-      popupInput2.classList.add('popup__input-item_invalid');
-      inputError2.textContent = popupInput2.validationMessage;
-      if (popupInput2.validationMessage === ''){
-        popupInput2.classList.remove('popup__input-item_invalid');
-      }
-    }   
+    if(inputs !== popupForm){
+      inputError1.textContent = '';
+      inputError2.textContent = '';
+      popupInput1.classList.remove('popup__input-item_invalid')
+      popupInput2.classList.remove('popup__input-item_invalid');
+      if(document.activeElement=== popupInput1 || document.activeElement=== popupInput2){
+        popupInput1.classList.add('popup__input-item_invalid');
+        inputError1.textContent = popupInput1.validationMessage;
+        if (popupInput1.validationMessage === ''){
+          popupInput1.classList.remove('popup__input-item_invalid');
+        }
+        popupInput2.classList.add('popup__input-item_invalid');
+        inputError2.textContent = popupInput2.validationMessage;
+        if (popupInput2.validationMessage === ''){
+          popupInput2.classList.remove('popup__input-item_invalid');
+        }
+      }   
+    }
   }
   /*тут к сожалению получается так что классы добавляются
   при каждом изменении поля инпута, но к сожалению более изящного решения придуумать не удалось.*/
